8826f726a3cc21ab71cb6e6ca275b98b8c99ac41
Yimeng Shan <121172737+YmShan@users.noreply.github.com>
2025-03-15 10:22:37 +0800

    Update train_tiny_visevent.sh

--- train_tiny_visevent.sh
+++ train_tiny_visevent.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=4,5,6 python tracking/train.py --script SDTrack --config SDTrack-tiny-visevent --save_dir ./output --mode multiple --nproc_per_node 3 --use_wandb 0
+CUDA_VISIBLE_DEVICES=0,1,2,3 python tracking/train.py --script SDTrack --config SDTrack-tiny-visevent --save_dir ./output --mode multiple --nproc_per_node 4 --use_wandb 0
