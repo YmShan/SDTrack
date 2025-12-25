a18d147266c5b3fc9422f648505ba2838def39d9
Yimeng Shan <121172737+YmShan@users.noreply.github.com>
2025-12-25 08:55:41 +0800

    Update SDTrack.py

--- SDTrack-Event/lib/models/SDTrack/SDTrack.py
+++ SDTrack-Event/lib/models/SDTrack/SDTrack.py
@@ -9,17 +9,20 @@ import torch
 from torch import nn
 from torch.nn.modules.transformer import _get_clones
 
-from lib.models.layers.head import build_box_head
 
 from lib.utils.box_ops import box_xyxy_to_cxcywh
 from lib.models.SDTrack.SDTrack_tiny_model import SDTrack_tiny as tiny
+
+from lib.models.SDTrack.SDTrack_tiny_LIF_T4D1_model import SDTrack_tiny_T4D1 as tiny_T4D1
+from lib.models.SDTrack.SDTrack_tiny_T2D2_model import SDTrack_tiny_T2D2 as tiny_T2D2
+
 from lib.models.SDTrack.SDTrack_base_model import SDTrack_base as base
 from lib.train.admin import env_settings  # 导入环境设置
 
 class SDTrack(nn.Module):
     """ This is the base class for SDTrack """
 
-    def __init__(self, transformer, box_head, aux_loss=False, head_type="CORNER"):
+    def __init__(self, transformer, box_head, aux_loss=False, head_type="CORNER", cfg=None):
         """ Initializes the model.
         Parameters:
             transformer: torch module of the transformer architecture.
@@ -28,7 +31,7 @@ class SDTrack(nn.Module):
         super().__init__()
         self.backbone = transformer
         self.box_head = box_head
-
+        self.cfg = cfg
         self.aux_loss = aux_loss
         self.head_type = head_type
         if head_type == "CORNER" or head_type == "CENTER":
@@ -42,51 +45,91 @@ class SDTrack(nn.Module):
                 search: torch.Tensor,
 
                 ):
-        x, aux_dict = self.backbone(z=template, x=search)
+        if self.cfg.MODEL.T == 1:
+            x, aux_dict = self.backbone(z=template, x=search)
 
-        # Forward head
-        feat_last = x               # [1, 768, 320]
-        if isinstance(x, list):
-            feat_last = x[-1]
-        feat_last = feat_last.permute(0, 2, 1)
-        out = self.forward_head(feat_last, None)
+            # Forward head
+            feat_last = x               # [1, 768, 320]
+            if isinstance(x, list):
+                feat_last = x[-1]
+            feat_last = feat_last.permute(0, 2, 1)
+            out = self.forward_head(feat_last, None)
 
-        out.update(aux_dict)
-        out['backbone_feat'] = x
-        return out
+            out.update(aux_dict)
+            out['backbone_feat'] = x
+            return out
+        else:
+            x, aux_dict = self.backbone(z=template, x=search)
+            # Forward head
+            feat_last = x               # [1, 768, 320]
+            if isinstance(x, list):
+                feat_last = x[-1]
+            feat_last = feat_last.permute(0, 1, 3, 2)
+            out = self.forward_head(feat_last, None)
+
+            out.update(aux_dict)
+            out['backbone_feat'] = x
+            return out
 
     def forward_head(self, cat_feature, gt_score_map=None):
         """
         cat_feature: output embeddings of the backbone, it can be (HW1+HW2, B, C) or (HW2, B, C)
         """
-        enc_opt = cat_feature[:, -self.feat_len_s:]         # [B, 256, 768]
-        opt = (enc_opt.unsqueeze(-1)).permute((0, 3, 2, 1)).contiguous()
-        bs, Nq, C, HW = opt.size()
-        opt_feat = opt.view(-1, C, self.feat_sz_s, self.feat_sz_s)                  # [B, 320, 16, 16]
+        if self.cfg.MODEL.T == 1:
+            enc_opt = cat_feature[:, -self.feat_len_s:]         # [B, 256, 768]
+            opt = (enc_opt.unsqueeze(-1)).permute((0, 3, 2, 1)).contiguous()
+            bs, Nq, C, HW = opt.size()
+            opt_feat = opt.view(-1, C, self.feat_sz_s, self.feat_sz_s)                  # [B, 320, 16, 16]
 
-        if self.head_type == "CORNER":
-            # run the corner head
-            pred_box= self.box_head(opt_feat)
-            outputs_coord = box_xyxy_to_cxcywh(pred_box)
-            outputs_coord_new = outputs_coord.view(bs, Nq, 4)
-            out = {'pred_boxes': outputs_coord_new
-                   }
-            return out
+            if self.head_type == "CORNER":
+                # run the corner head
+                pred_box= self.box_head(opt_feat)
+                outputs_coord = box_xyxy_to_cxcywh(pred_box)
+                outputs_coord_new = outputs_coord.view(bs, Nq, 4)
+                out = {'pred_boxes': outputs_coord_new
+                    }
+                return out
 
-        elif self.head_type == "CENTER":
-            # run the center head
-            score_map_ctr, bbox, size_map, offset_map = self.box_head(opt_feat, gt_score_map)
-            # outputs_coord = box_xyxy_to_cxcywh(bbox)
-            outputs_coord = bbox
-            outputs_coord_new = outputs_coord.view(bs, Nq, 4)
-            out = {'pred_boxes': outputs_coord_new,
-                   'score_map': score_map_ctr,
-                   'size_map': size_map,
-                   'offset_map': offset_map}
-            return out
+            elif self.head_type == "CENTER":
+                # run the center head
+                score_map_ctr, bbox, size_map, offset_map = self.box_head(opt_feat, gt_score_map)
+                # outputs_coord = box_xyxy_to_cxcywh(bbox)
+                outputs_coord = bbox
+                outputs_coord_new = outputs_coord.view(bs, Nq, 4)
+                out = {'pred_boxes': outputs_coord_new,
+                    'score_map': score_map_ctr,
+                    'size_map': size_map,
+                    'offset_map': offset_map}
+                return out
+            else:
+                raise NotImplementedError
         else:
-            raise NotImplementedError
+            enc_opt = cat_feature[:, :, -self.feat_len_s:]          # [2, B, 256, 320]
+            opt = (enc_opt.unsqueeze(-1)).permute((0, 1, 4, 3, 2)).contiguous()
+            T, bs, Nq, C, HW = opt.size()
+            opt_feat = opt.view(T, -1, C, self.feat_sz_s, self.feat_sz_s)                  # [B, 320, 16, 16]
 
+            if self.head_type == "CORNER":
+                # run the corner head
+                pred_box= self.box_head(opt_feat)
+                outputs_coord = box_xyxy_to_cxcywh(pred_box)
+                outputs_coord_new = outputs_coord.view(bs, Nq, 4)
+                out = {'pred_boxes': outputs_coord_new
+                    }
+                return out
+            elif self.head_type == "CENTER":
+                # run the center head
+                score_map_ctr, bbox, size_map, offset_map = self.box_head(opt_feat, gt_score_map)
+                # outputs_coord = box_xyxy_to_cxcywh(bbox)
+                outputs_coord = bbox
+                outputs_coord_new = outputs_coord.view(bs, Nq, 4)
+                out = {'pred_boxes': outputs_coord_new,
+                    'score_map': score_map_ctr,
+                    'size_map': size_map,
+                    'offset_map': offset_map}
+                return out
+            else:
+                raise NotImplementedError
 
 # 计算模型的参数量
 def count_parameters(model):
@@ -109,6 +152,27 @@ def build_SDTrack(cfg, training=True):
         hidden_dim = 360            # lite 360    base 768
         patch_start_index = 1
         checkpoint = torch.load(env_settings().pretrained_networks + '/SDTrack-tiny-1x4.pth', map_location="cpu")
+        from lib.models.layers.head import build_box_head
+        box_head = build_box_head(cfg, hidden_dim)
+
+
+    elif cfg.MODEL.BACKBONE.TYPE == 'tiny_T4D1':
+        backbone = tiny_T4D1()
+
+        hidden_dim = 360            # lite 360    base 768
+        patch_start_index = 1
+        checkpoint = torch.load(env_settings().pretrained_networks + '/SDTrack-tiny-4x1.pth', map_location="cpu")
+        from lib.models.layers.head_T4D1 import build_box_head
+        box_head = build_box_head(cfg, hidden_dim)
+
+    elif cfg.MODEL.BACKBONE.TYPE == 'tiny_T2D2':
+        backbone = tiny_T2D2()
+
+        hidden_dim = 360            # lite 360    base 768
+        patch_start_index = 1
+        checkpoint = torch.load(env_settings().pretrained_networks + '/SDTrack-tiny-2x2.pth', map_location="cpu")
+        from lib.models.layers.head_T2D2 import build_box_head
+        box_head = build_box_head(cfg, hidden_dim)
 
     elif cfg.MODEL.BACKBONE.TYPE == 'base':
         backbone = base()
@@ -117,19 +181,22 @@ def build_SDTrack(cfg, training=True):
         patch_start_index = 1
 
         checkpoint = torch.load(env_settings().pretrained_networks + '/SDTrack-base-1x4.pth', map_location="cpu")
+        from lib.models.layers.head import build_box_head
+        box_head = build_box_head(cfg, hidden_dim)
+
 
     else:
         raise NotImplementedError
 
     # backbone.finetune_track(cfg=cfg, patch_start_index=patch_start_index)
 
-    box_head = build_box_head(cfg, hidden_dim)
 
     model = SDTrack(
         backbone,
         box_head,
         aux_loss=False,
         head_type=cfg.MODEL.HEAD.TYPE,
+        cfg = cfg
     )
 
     # print(model)
